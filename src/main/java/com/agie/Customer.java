d068a3681874d58f9304e484eb66ffea947d2ead
Gusten <115186831+Netsug@users.noreply.github.com>
2023-10-19 15:57:12 +0200

    isleapyear is comment added

--- src/main/java/com/agie/Customer.java
+++ src/main/java/com/agie/Customer.java
@@ -90,6 +90,7 @@ public class Customer {
         return Integer.parseInt(pnrDay) <= daysInMonth;
     }
 
+    // is not supposed to be public. public for testing purposes
     public boolean isYearALeapYear(int year) {
         return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
