254e796dad95152faceb132b109e9d0910ae0894
Gusten <115186831+Netsug@users.noreply.github.com>
2023-10-19 15:02:09 +0200

    Customer fixed again

--- src/main/java/com/agie/Customer.java
+++ src/main/java/com/agie/Customer.java
@@ -2,12 +2,13 @@ package com.agie;
 import java.util.Calendar;
 
 
-// A personNumber is a 10-digit number that is used to identify a person in Sweden.
-// In this implementation we've decided to use 12 digits instead of 10.
-// This makes the format as following
-// YYYYMMDDXXXX
-// The first 8 digits are the date of birth.
-// The last 4 digits are a serial number that is used to differentiate people born on the same day.
+/* A personNumber is a 10-digit number that is used to identify a person in Sweden.
+ In this implementation we've decided to use 12 digits instead of 10.
+ This makes the format as following
+ YYYYMMDDXXXX
+ The first 8 digits are the date of birth.
+ The last 4 digits are a serial number that is used to differentiate people born on the same day.
+ */
 
 public class Customer {
     private final long pnr;
@@ -18,35 +19,34 @@ public class Customer {
     private final boolean isLeapYear;
 
     public Customer(long personNumber){
+        if(personNumber < 0){
+            throw new IllegalArgumentException("personNumber can't be negative");
+        }
 
-        try{
-            pnrString = Integer.toString(Math.toIntExact(personNumber)); // Unure about math.toint
-        } catch (NumberFormatException e){
-            throw new IllegalArgumentException("Personnummer can only contain numbers");
+        pnrString = Long.toString(personNumber);
+
+        if(!isLengthOfPnrValid()){
+            throw new IllegalArgumentException("Personnummer has to be 12 in length");
+        }
+        if(isDateInFuture()){
+            throw new IllegalArgumentException("Personnummer can't be in the future");
         }
 
         pnrYear = pnrString.substring(0, 4);
         pnrMonth = pnrString.substring(4, 6);
         pnrDay = pnrString.substring(6, 8);
+        // We can't validate the last 4 digits.
 
+        isLeapYear = isYearALeapYear(Integer.parseInt(pnrYear));
 
-        isLeapYear = isYearALeapYear();
-
-        if(!isLengthOfPnrValid()){
-            throw new IllegalArgumentException("Personnummer has to be 12 in length");
+        if(!isDayInMonthValid()){
+            throw new IllegalArgumentException("Day is not valid for the month");
         }
-
-        if(isDateInFuture()){
-            throw new IllegalArgumentException("Personnummer can't be in the future");
-        }
-
-        isDayInMonthValid();
 
         this.pnr = personNumber;
     }
 
     private boolean isLengthOfPnrValid() {
-        // TODO: negativa personnummer är tillåtna rn. Fix
         return pnrString.length() == 12;
     }
 
@@ -67,7 +67,6 @@ public class Customer {
                 return day > calendar.get(Calendar.DAY_OF_MONTH);
             }
         }
-
         return false;
     }
 
@@ -92,15 +91,8 @@ public class Customer {
         return Integer.parseInt(pnrDay) <= daysInMonth;
     }
 
-    private boolean isYearALeapYear() {
-        int year = Integer.parseInt(pnrYear);
-        if(year % 4 == 0){
-            if(year % 100 == 0){
-                return year % 400 == 0; // no idea if this last part is needed
-            }
-            return false;
-        }
-        return false;
+    private boolean isYearALeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 
     public long getPnr(){
