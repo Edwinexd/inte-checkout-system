6ce5e03d32eaa2a53bec50c494d52f4e6f9d9797
Edwin <60476129+Edwinexd@users.noreply.github.com>
2023-10-20 15:21:39 +0200

    remove action test code

--- src/main/java/com/agie/LoggingException.java
+++ src/main/java/com/agie/LoggingException.java
@@ -6,8 +6,4 @@ public class LoggingException extends Exception {
     public LoggingException(String message, IOException e) {
         super(message, e);
     }
-
-    public void doNothing() {
-        System.out.println("do nothing");
-    }
 }
