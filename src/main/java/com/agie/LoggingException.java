ce8e9d3f949a5fcb749c74a85c8d9313d86caf06
Edwin <60476129+Edwinexd@users.noreply.github.com>
2023-10-20 15:18:01 +0200

    Test coverage reporting

--- src/main/java/com/agie/LoggingException.java
+++ src/main/java/com/agie/LoggingException.java
@@ -6,4 +6,8 @@ public class LoggingException extends Exception {
     public LoggingException(String message, IOException e) {
         super(message, e);
     }
+
+    public void doNothing() {
+        System.out.println("do nothing");
+    }
 }
