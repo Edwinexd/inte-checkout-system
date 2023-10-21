ce5051936e7be2f4522bed67fd7be791d8a2830b
Edwin <60476129+Edwinexd@users.noreply.github.com>
2023-10-21 10:52:48 +0200

    Refactor Customer + Add customer tests for leap year

--- src/main/java/com/agie/Customer.java
+++ src/main/java/com/agie/Customer.java
@@ -1,4 +1,5 @@
 package com.agie;
+
 import java.util.Calendar;
 
 /* A personNumber is a 10-digit number that is used to identify a person in Sweden.
@@ -17,84 +18,82 @@ public class Customer {
     private final String pnrDay;
     private final boolean isLeapYear;
 
-    public Customer(long personNumber){
-        if(personNumber < 0){
+    public Customer(long personNumber) {
+        if (personNumber < 0) {
             throw new IllegalArgumentException("personNumber can't be negative");
         }
-        if(!isLengthOfPnrValid(personNumber)){
+        if (!isLengthOfPnrValid(personNumber)) {
             throw new IllegalArgumentException("Personnummer has to be 12 in length");
         }
 
         this.pnr = personNumber;
-        pnrString = ""+personNumber;
+        pnrString = "" + personNumber;
 
-        pnrYear = pnrString.substring(0,4);
+        pnrYear = pnrString.substring(0, 4);
         pnrMonth = pnrString.substring(4, 6);
         pnrDay = pnrString.substring(6, 8);
         // We can't validate the last 4 digits.
 
-        if(isDateInFuture()){
+        if (isDateInFuture(Integer.parseInt(pnrYear), Integer.parseInt(pnrMonth), Integer.parseInt(pnrDay))) {
             throw new IllegalArgumentException("Personnummer can't be in the future");
         }
 
         isLeapYear = isYearALeapYear(Integer.parseInt(pnrYear));
 
-        if(!isDayInMonthValid()){
+        if (!isDayInMonthValid(isLeapYear, Integer.parseInt(pnrMonth), Integer.parseInt(pnrDay))) {
             throw new IllegalArgumentException("Day is not valid for the month");
         }
     }
 
-    private boolean isLengthOfPnrValid(Long personNumber){
-        String pnrString = ""+personNumber;
+    private boolean isLengthOfPnrValid(Long personNumber) {
+        String pnrString = "" + personNumber;
         return pnrString.length() == 12;
     }
 
-    private boolean isDateInFuture(){
+    private boolean isDateInFuture(int year, int month, int day) {
         Calendar calendar = Calendar.getInstance();
-        int year = Integer.parseInt(pnrYear);
-        int month = Integer.parseInt(pnrMonth);
-        int day = Integer.parseInt(pnrDay);
-
-        if(year > calendar.get(Calendar.YEAR)){
+        if (year > calendar.get(Calendar.YEAR)) {
             return true;
         }
-        if (year == calendar.get(Calendar.YEAR)){
-            if(month > calendar.get(Calendar.MONTH)){
-                return true;
-            }
-            if (month == calendar.get(Calendar.MONTH)){
-                return day > calendar.get(Calendar.DAY_OF_MONTH);
-            }
+        if (year < calendar.get(Calendar.YEAR)) {
+            return false;
         }
-        return false;
+
+        if (month > calendar.get(Calendar.MONTH)) {
+            return true;
+        }
+        if (month < calendar.get(Calendar.MONTH)) {
+            return false;
+        }
+
+        return day > calendar.get(Calendar.DAY_OF_MONTH);
     }
 
     // Checks if the day is valid for the month.
     // EXAMPLE: 31st of February is not a valid date.
     // EXAMPLE: 29th of February is only valid if the year is a leap year.
-    private boolean isDayInMonthValid() {
+    private boolean isDayInMonthValid(boolean isLeapYear, int month, int day) {
         int daysInMonth;
-        if(pnrMonth.equals("02")){
-            if (isLeapYear){
+        if (month == 2) {
+            if (isLeapYear) {
                 daysInMonth = 29;
-            }
-            else {
+            } else {
                 daysInMonth = 28;
             }
-        } else if (pnrMonth.equals("04") || pnrMonth.equals("06") || pnrMonth.equals("09") || pnrMonth.equals("11")){
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
             daysInMonth = 30;
         } else {
             daysInMonth = 31;
         }
 
-        return Integer.parseInt(pnrDay) <= daysInMonth;
+        return day <= daysInMonth;
     }
 
     private boolean isYearALeapYear(int year) {
         return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 
-    public long getPnr(){
+    public long getPnr() {
         return this.pnr;
     }
 }
